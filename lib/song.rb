5863e38392fc48113efeaddfe0bd3b0d71a3a9bb
Isabella Costabile <costabileisa@gmail.com>
2023-02-08 13:47:56 -0500

    commit tree 8af39f2af692f057596340a0563e29e3e1e2b271

---
+++ lib/song.rb
@@ -0,0 +1,52 @@
+class Song
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
+    end
+    
+    # getter
+    def name
+        @name
+    end
+    def artist
+        @artist
+    end
+    def genre
+        @genre
+    end
+    # setter
+    def name=(name)
+        @name = name
+    end
+    def artist=(artist)
+        @artist = artist
+    end
+    def genre=(genre)
+        @genre = genre
+    end
+
+    def self.count
+        @@count
+    end
+    def self.artists
+        @@artists.uniq
+    end
+    def self.genres
+        @@genres.uniq
+    end
+    def self.artist_count
+        @@artists.tally
+    end
+    def self.genre_count
+        @@genres.tally
+    end
+end
